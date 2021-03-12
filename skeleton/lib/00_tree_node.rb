bc42125c8223aef0c5922fa1e79af63d891abef4
jessicaUP <uphoff.jessica@gmail.com>
2021-03-12 16:02:35 -0500

    Initialize PolyTreeNode class and start parent=

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -1,3 +1,20 @@
+require 'byebug'
 class PolyTreeNode
- hi
+
+    attr_reader :parent, :children, :value
+ 
+    def initialize(value)
+        @value = value
+        @parent = nil
+        @children = []
+    end
+
+    def parent=(parent_node) #self = child_node 
+        orginal_parent = self.parent
+        @parent = parent_node #if self.parent == nil
+        orginal_parent.children.select { |ele| ele != self }
+        parent_node.children << self #if parent_node.children.include?(self)
+    end
+
 end
+
